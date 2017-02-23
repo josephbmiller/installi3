1ecdeab3520c3cab31e6a1d4581000b7c8cef18f
Joseph Miller <mill1630@purdue.edu>
2017-02-23 12:50:36 -0500

    bugfix

--- run.sh
+++ run.sh
@@ -2,10 +2,11 @@ apt-get -y install vim
 apt-get -y install terminator
 apt-get -y install feh
 sh installgohu.sh
-echo "bindsym $mod+c exec google-chrome" > ~/.i3/config
-echo "new_window pixel 0" > ~/.i3/config
-echo "hide_edge_borders both" > ~/.i3/config
-echo "exec feh --bg-scale ~/wolf.png"
+echo "bindsym $mod+c exec google-chrome" >> ~/.i3/config
+echo "new_window pixel 0" >> ~/.i3/config
+echo "hide_edge_borders both" >> ~/.i3/config
+echo "exec feh --bg-scale ~/wolf.png" >> ~/.i3/config
+mkdir ~/.config/terminator
 cp terminatorconfig ~/.config/terminator/config
 cp i3status.conf ~/.i3status.conf
 cp wolf.png ~/wolf.png
